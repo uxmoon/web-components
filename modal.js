3c3b4ebf938cd5a4726ea28bbf4dc365cd24ade0
Martin Luna <mluna@perceptyx.com>
2022-10-24 11:32:24 -0300

    Update modal structure
    Update modal css for backdrop and modal

--- modal.js
+++ modal.js
@@ -2,6 +2,32 @@ class Modal extends HTMLElement {
   constructor() {
     super()
     this.attachShadow({ mode: 'open' })
+    this.shadowRoot.innerHTML = /* html */ `
+      <style>
+        .backdrop {
+          position: fixed;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          background-color: rgba(0,0,0,0.75);
+          z-index: 10;
+        }
+        .modal {
+          position: fixed;
+          top: 15vh;
+          left: 25%;
+          width: 50%;
+          height: 30rem;
+          z-index: 100;
+          background-color: #fff;
+          border-radius: 4px;
+          box-shadow: 0 2px 8px rgba(0,0,0,0.25);
+        }
+      </style>
+      <div class="backdrop"></div>
+      <div class="modal"></div>
+    `;
   }
 }
 
