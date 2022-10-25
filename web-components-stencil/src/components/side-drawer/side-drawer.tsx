6647c861a897c0964199b7c2521d63fec7833ee3
Martin Luna <mluna@perceptyx.com>
2022-10-25 16:21:54 -0300

    Add State to toggle content tab

--- web-components-stencil/src/components/side-drawer/side-drawer.tsx
+++ web-components-stencil/src/components/side-drawer/side-drawer.tsx
@@ -1,4 +1,4 @@
-import { h, Component, Prop } from '@stencil/core'
+import { h, Component, Prop, State } from '@stencil/core'
 
 @Component({
   tag: 'wc-drawer',
@@ -7,6 +7,8 @@ import { h, Component, Prop } from '@stencil/core'
 })
 
 export class SideDrawer {
+  @State() showContactTab = false;
+
   @Prop({ reflect: true }) heading: string;
 
   @Prop({ reflect: true, mutable: true }) show: boolean;
@@ -16,22 +18,29 @@ export class SideDrawer {
     this.show = false;
   }
 
+  onContentChange(content: string) {
+    // console.log(content);
+    this.showContactTab = content === 'contact';
+  }
+
   render() {
     let mainContent = <slot />
-    mainContent = (
-      <div class='contact-information'>
-        <h2>Contact information</h2>
-        <p>You can reach us via phone or email</p>
-        <ul>
-          <li>Phone: 11 5555 3333</li>
-          <li>Email:
-            <a href="mailto:fake@mail.com">
-              fake@mail.com
-            </a>
-          </li>
-        </ul>
-      </div>
-    );
+    if (this.showContactTab) {
+      mainContent = (
+        <div class='contact-information'>
+          <h2>Contact information</h2>
+          <p>You can reach us via phone or email</p>
+          <ul>
+            <li>Phone: 11 5555 3333</li>
+            <li>Email:
+              <a href="mailto:fake@mail.com">
+                fake@mail.com
+              </a>
+            </li>
+          </ul>
+        </div>
+      );
+    }
     return (
       <aside>
         <header>
@@ -44,8 +53,18 @@ export class SideDrawer {
           </button>
         </header>
         <section class='drawer-tabs'>
-          <button class='active'>Navigation</button>
-          <button>Contact</button>
+          <button
+            class={!this.showContactTab ? 'active' : ''}
+            onClick={this.onContentChange.bind(this, 'nav')}
+          >
+            Navigation
+          </button>
+          <button
+            class={this.showContactTab ? 'active' : ''}
+            onClick={this.onContentChange.bind(this, 'contact')}
+          >
+            Contact
+          </button>
         </section>
         <main>
           {mainContent}
