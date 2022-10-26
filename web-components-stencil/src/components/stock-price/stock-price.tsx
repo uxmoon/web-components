2b17a22dd7b715841a6d89c3b9e60d503e800ec0
Martin Luna <mluna@perceptyx.com>
2022-10-26 11:01:55 -0300

    Fetch and show price from API

--- web-components-stencil/src/components/stock-price/stock-price.tsx
+++ web-components-stencil/src/components/stock-price/stock-price.tsx
@@ -1,4 +1,4 @@
-import { h, Host, Component } from '@stencil/core'
+import { h, Host, Component, State } from '@stencil/core'
 
 @Component({
   tag: 'wc-stock-price',
@@ -7,21 +7,38 @@ import { h, Host, Component } from '@stencil/core'
 })
 
 export class StockPrice {
+  @State() fetchedPrice: number;
 
   // method to fetch data
   onFormSubmit(event: Event) {
     event.preventDefault();
-    console.log('go... fetch!');
+
+    // alpha vantage API
+    const url = 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=IBM&apikey=demo'
+
+    fetch(url)
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        // console.log(data['Global Quote']['05. price']);
+
+        // get price and convert string to number
+        this.fetchedPrice = +data['Global Quote']['05. price']
+      })
+      .catch((err) => {
+        console.log(err);
+      })
   }
 
   render() {
     return (
       <Host>
-        <form onSubmit={this.onFormSubmit}>
+        <form onSubmit={this.onFormSubmit.bind(this)}>
           <input type="text" id="stock-symbol" />
           <button type="submit">Fetch</button>
         </form>
-        <p>Price: {0}</p>
+        <p>Price: ${this.fetchedPrice}</p>
       </Host>
     )
   }
