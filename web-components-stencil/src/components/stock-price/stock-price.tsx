1cccd2c509f2e04f83187d1d3ae0a157af61ac00
Martin Luna <mluna@perceptyx.com>
2022-10-28 14:26:45 -0300

    Add Watch for prop change

--- web-components-stencil/src/components/stock-price/stock-price.tsx
+++ web-components-stencil/src/components/stock-price/stock-price.tsx
@@ -1,4 +1,4 @@
-import { h, Host, Component, State, Prop } from '@stencil/core'
+import { h, Host, Component, State, Prop, Watch } from '@stencil/core'
 import { AV_API_KEY } from '../../global/global';
 
 @Component({
@@ -22,6 +22,13 @@ export class StockPrice {
 
   @Prop() stockSymbol: string;
 
+  @Watch('stockSymbol')
+  stockSymbolChange(newValue: string, oldValue: string) {
+    if (newValue !== oldValue) {
+      this.fetchStockPrice(newValue)
+    }
+  }
+
   // method to fetch data
   onFormSubmit(event: Event) {
     event.preventDefault();
@@ -31,10 +38,28 @@ export class StockPrice {
     this.fetchStockPrice(stockSymbol)
   }
 
+  componentWillLoad() {
+    console.log('component will load');
+    console.log(this.stockSymbol);
+  }
+
   componentDidLoad() {
-    if(this.stockSymbol) {
+    console.log('component did load');
+    if (this.stockSymbol) {
       this.fetchStockPrice(this.stockSymbol)
     }
+  }
+
+  componentWillUpdate() {
+    console.log('component will update');
+  }
+
+  componentDidUpdate() {
+    console.log('component did update');
+  }
+
+  disconnectedCallback() {
+    console.log('component did unload');
   }
 
   fetchStockPrice(stockSymbol) {
@@ -46,7 +71,7 @@ export class StockPrice {
         return response.json()
       })
       .then((data) => {
-          if (!data['Global Quote']['05. price']) {
+        if (!data['Global Quote']['05. price']) {
           throw new Error('Invalid symbol!')
         }
         this.error = null;
