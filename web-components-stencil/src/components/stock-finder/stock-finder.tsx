c87b2fa82d688f22c10b9026bef4a0226409baf1
Martin Luna <mluna@perceptyx.com>
2022-10-28 15:34:26 -0300

    Add method to fetch stock symbol

--- web-components-stencil/src/components/stock-finder/stock-finder.tsx
+++ web-components-stencil/src/components/stock-finder/stock-finder.tsx
@@ -1,4 +1,5 @@
 import { h, Host, Component } from '@stencil/core'
+import { AV_API_KEY } from '../../global/global';
 
 @Component({
   tag: 'wc-stock-finder',
@@ -7,12 +8,39 @@ import { h, Host, Component } from '@stencil/core'
 })
 
 export class StockFinder {
+  // Add ref
+  elStockName: HTMLInputElement;
+
+  // method
+  onFindStocks(event: Event) {
+    event.preventDefault();
+    const stockName = this.elStockName.value;
+    const url = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${stockName}&apikey=${AV_API_KEY}`
+    fetch(url)
+      .then((data) => {
+        return data.json()
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+
   render() {
     return (
       <Host>
-        <h2>
-          Stock finder
-        </h2>
+        <form onSubmit={this.onFindStocks.bind(this)}>
+          <input
+            type="text"
+            id="stock-symbol"
+            ref={(el) => this.elStockName = el}
+          />
+          <button
+            type="submit"
+          >Find</button>
+        </form>
       </Host>
     )
   }
